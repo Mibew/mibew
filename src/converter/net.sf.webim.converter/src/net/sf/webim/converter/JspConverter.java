46f4e47510acf7839884d7220e115e796cee74aa
Evgeny Gryaznov <inspirer@users.sourceforge.net>
2008-01-25 14:39:09 +0000

    initial idea to generate site from templates
    git-svn-id: https://webim.svn.sourceforge.net/svnroot/webim/trunk@44 c66351dc-e62f-0410-b875-e3a5c0b9693f

--- src/converter/net.sf.webim.converter/src/net/sf/webim/converter/JspConverter.java
+++ src/converter/net.sf.webim.converter/src/net/sf/webim/converter/JspConverter.java
@@ -1,22 +1,32 @@
 package net.sf.webim.converter;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
 
+import net.sf.lapg.templates.api.EvaluationContext;
+import net.sf.lapg.templates.api.impl.FolderTemplateLoader;
+import net.sf.lapg.templates.api.impl.TemplateEnvironment;
 import net.sf.lapg.templates.model.xml.XmlModel;
+import net.sf.lapg.templates.model.xml.XmlNavigationFactory;
 import net.sf.lapg.templates.model.xml.XmlNode;
 
 public class JspConverter {
 
 	public static void main(String[] args) {
-		String toProcess = getFileContents("../test.xml");
+		String toProcess = getFileContents("test/index.xml");
 
 		XmlNode root = XmlModel.load(toProcess);
 
-		System.out.println(">>>\n" + root.toString() + "<<<");
-
+		ConverterHelper helper = new ConverterHelper();
+		TemplateEnvironment env = new TemplateEnvironment(
+				new XmlNavigationFactory(),
+				new FolderTemplateLoader(new File("templates")));
+		EvaluationContext context = new EvaluationContext(root.getChildren());
+		context.setVariable("helper", helper);
+		System.out.println(env.executeTemplate("conv.convertList", context, null));
 	}
 
 	private static String getFileContents(String file) {
