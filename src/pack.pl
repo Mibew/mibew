460efce20b2cebdf7908d6e14ee97dae6a8ea9dc
Evgeny Gryaznov <inspirer@users.sourceforge.net>
2008-05-06 22:32:46 +0000

    whatsnew line, pack.pl
    git-svn-id: https://webim.svn.sourceforge.net/svnroot/webim/trunk@66 c66351dc-e62f-0410-b875-e3a5c0b9693f

---
+++ src/pack.pl
@@ -0,0 +1,44 @@
+#!/usr/bin/perl
+
+##################################################################
+# Arguments
+##################################################################
+
+$targetFolder = "deploy";
+
+##################################################################
+# Copies tree into target folder, preprocess .phps
+##################################################################
+
+sub process_dir($$) {
+	my ($from,$to) = @_;
+	opendir(DIR, $from) || die "can't opendir $from: $!";
+    my @content = readdir(DIR);
+    closedir DIR;
+    mkdir $to;
+    
+	for(grep { -f "$from/$_" && ($_ !~ /^\./ || $_ eq ".htaccess") } @content) {
+		my ($source,$target) = ("$from/$_","$to/$_");
+
+		open (IN,"$source");
+		binmode(IN);
+		open (OUT,">$target");
+		binmode(OUT);
+		print OUT $buffer while (read (IN,$buffer,65536));
+	}
+	
+	for(grep { -d "$from/$_" && $_ !~ /^\./ } @content) {
+		process_dir("$from/$_","$to/$_");
+	} 
+}
+
+##################################################################
+# Main
+##################################################################
+
+die "Target folder exists: $targetFolder" if -e $targetFolder;
+
+process_dir("./webim", $targetFolder);
+
+chdir $targetFolder;
+`tar zcf ../webim.tar.gz *`;
