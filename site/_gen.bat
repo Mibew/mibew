83f8acd2f0643e983fd2c21c38f5470f465e5f7d
Evgeny Gryaznov <inspirer@users.sourceforge.net>
2008-10-21 21:52:53 +0000

    rm old
    git-svn-id: https://webim.svn.sourceforge.net/svnroot/webim/trunk@188 c66351dc-e62f-0410-b875-e3a5c0b9693f

--- site/_gen.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-xsltproc --xinclude --stringparam lang eng default.xslt index.xml > public_html\index.html
-xsltproc --xinclude --stringparam lang eng default.xslt download.xml > public_html\download.html
