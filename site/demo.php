0d5cc66450108b0f6548595266e49bccab48e1b9
Evgeny Gryaznov <inspirer@users.sourceforge.net>
2009-01-03 16:09:17 +0000

    temporary, demo on live-im.com
    git-svn-id: https://webim.svn.sourceforge.net/svnroot/webim/trunk@275 c66351dc-e62f-0410-b875-e3a5c0b9693f

--- site/demo.php
+++ site/demo.php
@@ -13,13 +13,13 @@ require_once('inc/menu.i');
 
 <?php echo getlocal("demo.click") ?><br/>
 <br/>
-<!-- webim button --><a href="/webim/client.php?locale=<?php echo $current_locale ?>" target="_blank" onclick="if(navigator.userAgent.toLowerCase().indexOf('opera') != -1 && window.event.preventDefault) window.event.preventDefault();this.newWindow = window.open('/webim/client.php?locale=<?php echo $current_locale ?>', 'webim', 'toolbar=0,scrollbars=0,location=0,status=1,menubar=0,width=640,height=480,resizable=1');this.newWindow.focus();this.newWindow.opener=window;return false;"><img src="/webim/button.php?image=webim&lang=<?php echo $current_locale ?>" border="0" width="163" height="61"/></a><!-- / webim button -->
+<!-- webim button --><a href="http://live-im.com/webim/client.php?locale=<?php echo $current_locale ?>" target="_blank" onclick="if(navigator.userAgent.toLowerCase().indexOf('opera') != -1 && window.event.preventDefault) window.event.preventDefault();this.newWindow = window.open('http://live-im.com/webim/client.php?locale=<?php echo $current_locale ?>', 'webim', 'toolbar=0,scrollbars=0,location=0,status=1,menubar=0,width=640,height=480,resizable=1');this.newWindow.focus();this.newWindow.opener=window;return false;"><img src="http://live-im.com/webim/button.php?image=webim&lang=<?php echo $current_locale ?>" border="0" width="163" height="61"/></a><!-- / webim button -->
 <br/>
 <br/>
 
 <?php echo getlocal("demo.login") ?><br/><br/>
 
-<a href="/webim/"><?php echo getlocal("demo.application") ?>, 1.5.0</a>
+<a href="http://live-im.com/webim/"><?php echo getlocal("demo.application") ?>, 1.5.0</a>
 <br/>
 <br/>
 <br/>
@@ -32,13 +32,13 @@ require_once('inc/menu.i');
 <div id="style1">
 <p>Default style</p>
 <p>
-<!-- webim button --><a href="/webim/client.php?locale=<?php echo $current_locale ?>&style=default" target="_blank" onclick="if(navigator.userAgent.toLowerCase().indexOf('opera') != -1 && window.event.preventDefault) window.event.preventDefault();this.newWindow = window.open('/webim/client.php?locale=<?php echo $current_locale ?>&style=default', 'webim', 'toolbar=0,scrollbars=0,location=0,status=1,menubar=0,width=640,height=480,resizable=1');this.newWindow.focus();this.newWindow.opener=window;return false;"><img src="images/style_default_tn.png" border="0"/></a><!-- / webim button -->
+<!-- webim button --><a href="http://live-im.com/webim/client.php?locale=<?php echo $current_locale ?>&style=default" target="_blank" onclick="if(navigator.userAgent.toLowerCase().indexOf('opera') != -1 && window.event.preventDefault) window.event.preventDefault();this.newWindow = window.open('http://live-im.com/webim/client.php?locale=<?php echo $current_locale ?>&style=default', 'webim', 'toolbar=0,scrollbars=0,location=0,status=1,menubar=0,width=640,height=480,resizable=1');this.newWindow.focus();this.newWindow.opener=window;return false;"><img src="images/style_default_tn.png" border="0"/></a><!-- / webim button -->
 </p>
 </div>
 <div id="style2">
 <p>Original style</p>
 <p>
-<!-- webim button --><a href="/webim/client.php?locale=<?php echo $current_locale ?>&style=original" target="_blank" onclick="if(navigator.userAgent.toLowerCase().indexOf('opera') != -1 && window.event.preventDefault) window.event.preventDefault();this.newWindow = window.open('/webim/client.php?locale=<?php echo $current_locale ?>&style=original', 'webim', 'toolbar=0,scrollbars=0,location=0,status=1,menubar=0,width=640,height=480,resizable=1');this.newWindow.focus();this.newWindow.opener=window;return false;"><img src="images/style_original_tn.png" border="0"/></a><!-- / webim button -->
+<!-- webim button --><a href="http://live-im.com/webim/client.php?locale=<?php echo $current_locale ?>&style=original" target="_blank" onclick="if(navigator.userAgent.toLowerCase().indexOf('opera') != -1 && window.event.preventDefault) window.event.preventDefault();this.newWindow = window.open('http://live-im.com/webim/client.php?locale=<?php echo $current_locale ?>&style=original', 'webim', 'toolbar=0,scrollbars=0,location=0,status=1,menubar=0,width=640,height=480,resizable=1');this.newWindow.focus();this.newWindow.opener=window;return false;"><img src="images/style_original_tn.png" border="0"/></a><!-- / webim button -->
 </p>
 </div>
 </div>
