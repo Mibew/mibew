53756d7747ca5520e6f9a34b787c4550f0405c08
Evgeny Gryaznov <inspirer@users.sourceforge.net>
2009-02-08 01:35:09 +0000

    do not get userAgent from db
    git-svn-id: https://webim.svn.sourceforge.net/svnroot/webim/trunk@352 c66351dc-e62f-0410-b875-e3a5c0b9693f

--- src/messenger/webim/operator/userhistory.php
+++ src/messenger/webim/operator/userhistory.php
@@ -35,7 +35,7 @@ function threads_by_userid($userid) {
 	$link = connect();
 
 	$query = sprintf("select unix_timestamp(dtmcreated) as created, unix_timestamp(dtmmodified) as modified, ".
-			 " threadid, remote, agentName, userName, userAgent ".
+			 " threadid, remote, agentName, userName ".
 			 "from chatthread ".
 			 "where userid=\"$userid\" order by created DESC", $userid);
 
