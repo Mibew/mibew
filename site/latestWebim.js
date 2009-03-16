b7b4e110ca0e3fca5ea4395c9d16ce14037d412f
Evgeny Gryaznov <inspirer@users.sourceforge.net>
2009-03-16 23:50:07 +0000

    news
    git-svn-id: https://webim.svn.sourceforge.net/svnroot/webim/trunk@417 c66351dc-e62f-0410-b875-e3a5c0b9693f

---
+++ site/latestWebim.js
@@ -0,0 +1,21 @@
+
+window.webimLatest = {
+	version: "1.5.2",
+	download: "http://openwebim.org/download.php",
+	title: "Open Web Messenger 1.5.2"
+};
+
+window.webimNews = [
+	{
+		title: 'Open Web Messenger 1.5.2 Released',
+		link: 'http://openwebim.org/forums/index.php?topic=52.0',
+		date: 'February 16, 2009',
+		message: 'We are pleased to announce the immediate availability of 1.5.2. This is a service release with minor fixes and improvements.'
+	},
+	{
+		title: 'Visit our forum',
+		link: 'http://openwebim.org/forums/',
+		date: 'January 15, 2009',
+		message: 'Welcome to our Web Messenger Community forum. We are ready to answer your questions.'
+	}
+];
