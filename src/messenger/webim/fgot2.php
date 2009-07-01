c5f4467b9d2e33285b32834a51fac59adb812a20
Ed Kraus <eddybaur@users.sourceforge.net>
2009-07-01 15:08:12 +0000

    a start on the forgot password site :)
    git-svn-id: https://webim.svn.sourceforge.net/svnroot/webim/trunk@564 c66351dc-e62f-0410-b875-e3a5c0b9693f

---
+++ src/messenger/webim/fgot2.php
@@ -0,0 +1,12 @@
+<?php
+
+	 $to = $_POST['email'];
+	 $subj = "Forgotten Password Retrival";
+	$password = $_POST['password'];
+	$username= $_POST['username'];
+	$message = "This message has been sent to you because a form was filled out saying you've forgotten your password.
+	Your username:$username 
+	Your new password:$password 
+	click here to confirm the changes."
+Mail($to, $subj, $message);
+	?>
