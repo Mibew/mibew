4547f84767c15d0ba8cd9c4f697eab5db0e64951
Ed Kraus <eddybaur@users.sourceforge.net>
2009-07-03 17:59:12 +0000

    donations button added. Inspirer we need 2 talk about that
    git-svn-id: https://webim.svn.sourceforge.net/svnroot/webim/trunk@576 c66351dc-e62f-0410-b875-e3a5c0b9693f

--- site/index.php
+++ site/index.php
@@ -18,6 +18,7 @@ require_once('inc/menu.i');
 			<div class="entry">
 				<p><?php echo getlocal("index.how.text") ?></p>
 				<p><?php echo getlocal("index.license") ?></p>
+
 			</div>
 			<div class="nometa"></div>
 		</div>
@@ -53,7 +54,14 @@ require_once('inc/locales.i');
 			<li>
 				<h2><?php echo getlocal("sidebar.quicknav") ?></h2>
 				<ul>
-					<li><a href="features.php"><?php echo getlocal("menu.features") ?></a></li>
+					<li><a href="features.php"><?php echo getlocal("menu.features") ?></a></li><p></p>
+                    <form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+<input type="hidden" name="cmd" value="_s-xclick">
+<input type="hidden" name="hosted_button_id" value="4710959">
+<input type="image" src="https://www.paypal.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+<img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1">
+</form>
+
 				</ul>
 			</li>
 		</ul>
