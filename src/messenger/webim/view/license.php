08d2d830c6df9afe99e3000f56dd36447f0869f7
Evgeny Gryaznov <inspirer@users.sourceforge.net>
2009-08-04 15:00:48 +0000

    combined license
    git-svn-id: https://webim.svn.sourceforge.net/svnroot/webim/trunk@601 c66351dc-e62f-0410-b875-e3a5c0b9693f

---
+++ src/messenger/webim/view/license.php
@@ -0,0 +1,42 @@
+<?php
+/*
+ * This file is part of Mibew Messenger project.
+ *
+ * Copyright (c) 2005-2009 Mibew Messenger Community
+ * All rights reserved. The contents of this file are subject to the terms of
+ * the Eclipse Public License v1.0 which accompanies this distribution, and
+ * is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * the GNU General Public License Version 2 or later (the "GPL"), in which case
+ * the provisions of the GPL are applicable instead of those above. If you wish
+ * to allow use of your version of this file only under the terms of the GPL, and
+ * not to allow others to use your version of this file under the terms of the
+ * EPL, indicate your decision by deleting the provisions above and replace them
+ * with the notice and other provisions required by the GPL.
+ *
+ * Contributors:
+ *    Evgeny Gryaznov - initial API and implementation
+ */
+
+$page['title'] = getlocal("license.title");
+$page['no_right_menu'] = true;
+$page['fixedwrap'] = true;
+
+function tpl_content() { global $page, $webimroot, $errors;
+?>
+
+<p>Mibew Messenger is currently available for use in all personal or commercial projects under both EPL and GPL
+licenses. This means that you can choose the license that best suits your project, and use it accordingly.</p>
+
+<br/>
+<p>
+<a href="<?php echo $webimroot ?>/epl-v10.html">EPL License</a><br/>
+<a href="<?php echo $webimroot ?>/gpl-2.0.txt">GPL License</a>
+</p>
+
+<?php 
+} /* content */
+
+require_once('inc_main.php');
+?>
