003fd4597ab1ef6ff499bf106467ca825b0fdba5
Evgeny Gryaznov <inspirer@users.sourceforge.net>
2009-08-04 13:38:37 +0000

    add GPL clause
    git-svn-id: https://webim.svn.sourceforge.net/svnroot/webim/trunk@599 c66351dc-e62f-0410-b875-e3a5c0b9693f

--- src/messenger/webim/libs/chat.php
+++ src/messenger/webim/libs/chat.php
@@ -8,6 +8,14 @@
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
+ * Alternatively, the contents of this file may be used under the terms of
+ * the GNU General Public License Version 2 or later (the "GPL"), in which case
+ * the provisions of the GPL are applicable instead of those above. If you wish
+ * to allow use of your version of this file only under the terms of the GPL, and
+ * not to allow others to use your version of this file under the terms of the
+ * EPL, indicate your decision by deleting the provisions above and replace them
+ * with the notice and other provisions required by the GPL.
+ *
  * Contributors:
  *    Evgeny Gryaznov - initial API and implementation
  *    Pavel Petroshenko - history search
