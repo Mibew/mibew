6bd70e088fdefbbb4d9ae7afe121b3316ada1f66
Evgeny Gryaznov <inspirer@users.sourceforge.net>
2009-08-11 11:08:18 +0000

    fix ie6 layout
    git-svn-id: https://webim.svn.sourceforge.net/svnroot/webim/trunk@623 c66351dc-e62f-0410-b875-e3a5c0b9693f

--- src/messenger/webim/js/162/ie.js
+++ src/messenger/webim/js/162/ie.js
@@ -3,9 +3,9 @@ window.attachEvent('onresize', mkwidth);
 
 function mkwidth(){
 	if(document.getElementById("wrap700")) {
-		document.getElementById("wrap700").style.width = document.documentElement.clientWidth < 700 ? "750px" : "100%";
+		document.getElementById("wrap700").style.width = document.documentElement.clientWidth < 750 ? "750px" : "100%";
 	}
 	if(document.getElementById("wrap400")) {
-		document.getElementById("wrap400").style.width = document.documentElement.clientWidth < 400 ? "450px" : "100%";
+		document.getElementById("wrap400").style.width = document.documentElement.clientWidth < 450 ? "450px" : "100%";
 	}
 };
