eedb94d05a9498c791da134a86f3fd863e25915a
Ed Kraus <eddybaur@users.sourceforge.net>
2009-11-18 04:00:20 +0000

    Removing COpywrite
    git-svn-id: https://webim.svn.sourceforge.net/svnroot/webim/trunk@701 c66351dc-e62f-0410-b875-e3a5c0b9693f

---
+++ site/copywrite.php
@@ -0,0 +1,42 @@
+<?php
+$page = 'home';
+$subpage = 'copywrite';
+require_once('libs/common.php');
+start_html_output();
+$title = getlocal("copywrite.title");
+require_once('inc/header.i');
+require_once('inc/menu.i');
+?>
+
+<div id="page">
+	<div id="content">
+		<div class="box1">
+			<p><img src="images/webimlogo.gif" alt="" width="74" height="79" class="left" /><?php echo getlocal("head.intro") ?></p>
+		</div>
+		<div class="post">
+			<h2 class="title"><?php echo getlocal("copywrite.title") ?></h2>
+			<div class="entry">
+			<? echo getlocal("copywrite.text") ?>
+			
+
+			
+			
+			</div>
+		</div>
+	</div>
+	<!-- end content -->
+	<!-- start sidebar -->
+	<div id="sidebar">
+		<ul>
+<?php
+require_once('inc/main.i');
+require_once('inc/locales.i');
+?>
+		</ul>
+	</div>
+	<!-- end sidebar -->
+	<div style="clear: both;">&nbsp;</div>
+</div>
+
+
+<?php require_once('inc/footer.i'); ?>
