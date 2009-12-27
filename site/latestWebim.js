c64d4d352579753e94564c110a4c1f06225a4439
Evgeny Gryaznov <inspirer@users.sourceforge.net>
2009-12-27 20:49:48 +0000

    update news
    git-svn-id: https://webim.svn.sourceforge.net/svnroot/webim/trunk@711 c66351dc-e62f-0410-b875-e3a5c0b9693f

--- site/latestWebim.js
+++ site/latestWebim.js
@@ -10,7 +10,7 @@ window.webimNews = [
 		title: 'Mibew Messenger 1.6.3 Released',
 		link: 'http://mibew.org/forums/index.php?topic=318.0',
 		date: 'October 14, 2009',
-		message: 'We finally released it! This is a service release which improves current functionality.'
+		message: 'We are pleased to announce the immediate availability of 1.6.3. This is a service release with minor fixes and improvements.'
 	},
 
 	{
@@ -18,12 +18,6 @@ window.webimNews = [
 		link: 'http://mibew.org/forums/index.php?topic=256.0',
 		date: 'August 12, 2009',
 		message: 'We finally released it! This is a service release which improves current functionality.'
-	},
-	{
-		title: 'Mibew Messenger 1.6.1 Released',
-		link: 'http://mibew.org/forums/index.php?topic=205.0',
-		date: 'June 5, 2009',
-		message: 'We are pleased to announce the immediate availability of 1.6.1. This is a service release with minor fixes and improvements.'
 	},
 	{
 		title: 'Visit our forum',
