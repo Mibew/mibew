860a5587944ca142513404c945fcb9aa23572643
Evgeny Gryaznov <egryaznov@gmail.com>
2011-02-16 01:26:02 +0100

    create button.php (for compatibility)

---
+++ src/messenger/webim/button.php
@@ -0,0 +1,21 @@
+<?php
+/*
+ * This file is part of Mibew Messenger project.
+ * 
+ * Copyright (c) 2005-2011 Mibew Messenger Community
+ * All rights reserved. The contents of this file are subject to the terms of
+ * the Eclipse Public License v1.0 which accompanies this distribution, and
+ * is available at http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Alternatively, the contents of this file may be used under the terms of
+ * the GNU General Public License Version 2 or later (the "GPL"), in which case
+ * the provisions of the GPL are applicable instead of those above. If you wish
+ * to allow use of your version of this file only under the terms of the GPL, and
+ * not to allow others to use your version of this file under the terms of the
+ * EPL, indicate your decision by deleting the provisions above and replace them
+ * with the notice and other provisions required by the GPL.
+ */
+
+require_once('b.php');
+exit;
+?>
