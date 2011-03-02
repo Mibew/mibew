f832e38e7538fb5b28b99e93db5a9d1bb4694e73
Evgeny Gryaznov <egryaznov@gmail.com>
2011-03-02 02:02:28 +0100

    fix sound issues in windows

--- src/messenger/webim/js/164/soundcheck.js
+++ src/messenger/webim/js/164/soundcheck.js
@@ -1,1 +1,1 @@
-pr.ir({'a#check-nv':function(vh){vh.onclick=function(){ys(wroot+'/sounds/new_user.wav');} ;} ,'a#check-nm':function(vh){vh.onclick=function(){ys(wroot+'/sounds/new_message.wav')} ;} } );
+pr.ir({'a#check-nv':function(bh){bh.onclick=function(){vs(wroot+'/sounds/new_user.wav');} ;} ,'a#check-nm':function(bh){bh.onclick=function(){vs(wroot+'/sounds/new_message.wav')} ;} } );
