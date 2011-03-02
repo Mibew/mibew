4257b6f87bfe46a82a81b041eeb60a5d7fcc8635
Evgeny Gryaznov <egryaznov@gmail.com>
2011-03-02 02:02:28 +0100

    fix sound issues in windows

--- src/messenger/webim/js/source/common.js
+++ src/messenger/webim/js/source/common.js
@@ -506,29 +506,14 @@ var CommonUtils = {
   }
 };
 
-function getWavMimeType() {
-   var mimeType = "application/x-mplayer2"; //default
-   var agt=navigator.userAgent.toLowerCase();
-   if (navigator.mimeTypes && agt.indexOf("windows")==-1) {
-      //non-IE, no-Windows
-      var plugin=navigator.mimeTypes["audio/mpeg"].enabledPlugin;
-      if (plugin || agt.indexOf("opera")>=0) mimeType="audio/mpeg";
-   }//end no-Windows
-   return mimeType;
-}
-
 function playSound(wav_file) {
-  var player = document.getElementById("player");
-  if (player)   {
-    player.parentNode.removeChild(player);
+  var player = document.createElement("div");
+  var agt = navigator.userAgent.toLowerCase();
+  if(agt.indexOf('opera') != -1) {
+  	player.style = "position: absolute; left: 0px; top: -200px;";
   }
-  player = document.body.appendChild(document.createElement("div"));
-  var agt=navigator.userAgent.toLowerCase();
-  if(agt.indexOf('safari') == -1 && agt.indexOf('windows') != -1) {
-    player.innerHTML = '<embed type="'+getWavMimeType()+'" src="'+wav_file+'" loop="0" autostart="1" width="0" height="0">';
-  } else {
-    player.innerHTML = '<div style="position: static; width: 0px; height: 0px"><embed type="'+getWavMimeType()+'" src="'+wav_file+'" hidden="true" loop="false" autostart="true"></div>';
-  }
+  document.body.appendChild(player);
+  player.innerHTML = '<embed src="'+wav_file+'" hidden="true" autostart="true" loop="false">';
 }
 
 function htmlescape(str) {
