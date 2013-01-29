dd79c9aa2171a7678df76df180a53fae3bf93810
Dmitriy Simushev <simushevds@ossg.ru>
2013-01-29 08:59:53 +0000

    Create base composite view

--- src/messenger/webim/js/source/default/collection_views/base_collection.js
+++ src/messenger/webim/js/source/default/collection_views/base_collection.js
@@ -9,6 +9,31 @@
 (function(Mibew, Backbone, _){
 
     /**
+     * Return special contructor for an item view if it exists or the
+     * default constructor otherwise. Use in Mibew.Views.CollectionBase and
+     * Mibew.Views.CompositeBase
+     * @private
+     * @param {Backbone.Model} item Collection item
+     * @param {Function} ItemViewType Default item view constructor
+     * @param {Object} itemViewOptions Additional item view options
+     * @returns Item view instance
+     */
+    var buildItemView = function(item, ItemViewType, itemViewOptions) {
+        // Build options object
+        var options = _.extend({model: item}, itemViewOptions);
+        // Try to find special view for this model
+        if (typeof item.getModelType != 'function') {
+            return new ItemViewType(options);
+        }
+        var modelType = item.getModelType();
+        if (modelType && Mibew.Views[modelType]) {
+            return new Mibew.Views[modelType](options);
+        } else {
+            return new ItemViewType(options);
+        }
+    }
+
+    /**
      * @class Represents base collection view
      */
     Mibew.Views.CollectionBase = Backbone.Marionette.CollectionView.extend(
@@ -23,25 +48,19 @@
             /**
              * Return special contructor for an item view if it exists or the
              * default constructor otherwise.
-             * @param {Backbone.Model} item Collection item
-             * @param {Function} ItemViewType Default item view constructor
-             * @param {Object} itemViewOptions Additional item view options
-             * @returns Item view instance
              */
-            buildItemView: function(item, ItemViewType, itemViewOptions) {
-                // Build options object
-                var options = _.extend({model: item}, itemViewOptions);
-                // Try to find special view for this model
-                if (typeof item.getModelType != 'function') {
-                    return new ItemViewType(options);
-                }
-                var modelType = item.getModelType();
-                if (modelType && Mibew.Views[modelType]) {
-                    return new Mibew.Views[modelType](options);
-                } else {
-                    return new ItemViewType(options);
-                }
-            }
+            buildItemView: buildItemView
+        }
+    );
+
+    Mibew.Views.CompositeBase = Backbone.Marionette.CompositeView.extend(
+        /** @lends Mibew.Views.CompositeBase.prototype */
+        {
+            /**
+             * Return special contructor for an item view if it exists or the
+             * default constructor otherwise.
+             */
+            buildItemView: buildItemView
         }
     );
 
