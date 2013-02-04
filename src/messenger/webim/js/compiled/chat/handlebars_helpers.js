b4f99c1c1832b2e7398c1f3b82c85267cc8ef533
Dmitriy Simushev <simushevds@ossg.ru>
2013-02-04 10:34:48 +0000

    Change JavaScript files structure
    Move message model and view from chat application
    to default application. Also move message template and
    'allowTags' Handlebars.js helper to default JavaScript application.

--- src/messenger/webim/js/compiled/chat/handlebars_helpers.js
+++
@@ -1,8 +0,0 @@
-/*
- This file is part of Mibew Messenger project.
- http://mibew.org
-
- Copyright (c) 2005-2011 Mibew Messenger Community
- License: http://mibew.org/license.php
-*/
-(function(b){b.registerHelper("allowTags",function(a){a=a.replace(/&lt;(span|strong)&gt;(.*?)&lt;\/\1&gt;/g,"<$1>$2</$1>");a=a.replace(/&lt;span class=&quot;(.*?)&quot;&gt;(.*?)&lt;\/span&gt;/g,'<span class="$1">$2</span>');return new b.SafeString(a)})})(Handlebars);
--- src/messenger/webim/js/source/default/handlebars_helpers.js
+++ src/messenger/webim/js/source/default/handlebars_helpers.js
@@ -40,6 +40,24 @@
         return new Handlebars.SafeString(result);
     });
 
+   /**
+    * Register 'allowTags' Handlebars helper.
+    *
+    * This helper unescape HTML entities for allowed (span and strong) tags.
+    */
+    Handlebars.registerHelper('allowTags', function(text) {
+        var result = text;
+        result = result.replace(
+            /&lt;(span|strong)&gt;(.*?)&lt;\/\1&gt;/g,
+            '<$1>$2</$1>'
+        );
+        result = result.replace(
+            /&lt;span class=&quot;(.*?)&quot;&gt;(.*?)&lt;\/span&gt;/g,
+            '<span class="$1">$2</span>'
+        );
+        return new Handlebars.SafeString(result);
+    });
+
     /**
      * Register 'formatTime' Handlebars helper.
      *
