efe6caee71b7e67def1bf3276c4debc672eec8bc
Fedor A. Fetisov <faf@ossg.ru>
2013-07-30 19:48:51 +0400

    Mibew Tray 1.1.1 release
    Update links to an actual mibew domain in Mibew Tray application (thanks to Nemesis0one)

--- src/mibewtray/MibewTray/options/About.cs
+++ src/mibewtray/MibewTray/options/About.cs
@@ -28,13 +28,23 @@ namespace webImTray {
         #endregion
 
         private void i_services_ru_link(object sender, LinkLabelLinkClickedEventArgs e) {
-            System.Diagnostics.Process.Start("http://openwebim.org/");
+            System.Diagnostics.Process.Start("http://mibew.org/");
         }
 
         private void webim_ru_link(object sender, LinkLabelLinkClickedEventArgs e) {
-            System.Diagnostics.Process.Start("http://openwebim.org/");
+            System.Diagnostics.Process.Start("http://mibew.org/");
         }
 
         public event ModifiedEvent PanelModified;
+
+        private void About_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void linkLabel3_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("http://mibew.org/forums/");
+        }
     }
 }
