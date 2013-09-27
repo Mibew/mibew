821de1baec38296721d9b96bf73eb4823ebb7025
Dmitriy Simushev <simushevds@ossg.ru>
2013-09-27 18:00:51 +0400

    Clean up repository

--- src/mibewjava/org.mibew.jabber/resources/packdmg.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-rm -rf *.dmg
-hdiutil create mibew-notifier-temp.dmg -volname "Mibew Notifier 1.0" -fs HFS+ -srcfolder "Mibew Notifier.app"
-hdiutil convert "mibew-notifier-temp.dmg" -format UDZO -imagekey zlib-level=9 -o "mibew-1.0.0.dmg"
-rm mibew-notifier-temp.dmg
