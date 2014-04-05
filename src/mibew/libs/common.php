727663d67c724da505273f1db7128b6d2f58931d
Fedor A. Fetisov <faf@ossg.ru>
2014-04-06 00:44:03 +0400

    Update version

--- src/mibew/libs/common.php
+++ src/mibew/libs/common.php
@@ -35,8 +35,8 @@ $home_locale = locale_pattern_check($home_locale) && locale_exists($home_locale)
 
 $locale_cookie_name = 'mibew_locale';
 
-$version = '1.6.10';
-$jsver = "1610";
+$version = '1.6.11';
+$jsver = "1611";
 
 // Make session cookie more secure
 @ini_set('session.cookie_httponly', TRUE);
