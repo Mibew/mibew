5ef2495ee35235d8786952e30bbfd40fa799e1c6
Dmitriy Simushev <simushevds@ossg.ru>
2014-05-20 09:22:44 +0000

    Replace "operator/cannededit.php" with a controller

--- src/mibew/libs/classes/Mibew/Controller/CannedMessageController.php
+++ src/mibew/libs/classes/Mibew/Controller/CannedMessageController.php
@@ -51,26 +51,21 @@ class CannedMessageController extends AbstractController
         }
         $page['locales'] = $locales_with_label;
 
-        $lang = $request->query->get('lang');
-        $correct_locale = $lang
-            && preg_match("/^[\w-]{2,5}$/", $lang)
-            && in_array($lang, $all_locales);
-        if (!$correct_locale) {
+        $lang = $this->extractLocale($request);
+        if (!$lang) {
             $lang = in_array(CURRENT_LOCALE, $all_locales)
                 ? CURRENT_LOCALE
                 : $all_locales[0];
         }
 
         // Get selected group ID, if any.
-        $group_id = $request->query->get('group');
-        if ($group_id && preg_match("/^\d{0,8}$/", $group_id)) {
+        $group_id = $this->extractGroupId($request);
+        if ($group_id) {
             $group = group_by_id($group_id);
             if (!$group) {
                 $page['errors'][] = getlocal('page.group.no_such');
                 $group_id = false;
             }
-        } else {
-            $group_id = false;
         }
 
         $all_groups = in_isolation($operator)
@@ -135,5 +130,162 @@ class CannedMessageController extends AbstractController
         );
 
         return $this->redirect($this->generateUrl('canned_message', $parameters));
+    }
+
+    /**
+     * Handles "canned_message_add" and "canned_message_edit" routes.
+     *
+     * Builds a page with form for add/edit canned message.
+     *
+     * @param Request $request
+     * @return string Rendered page content
+     */
+    public function showEditFormAction(Request $request)
+    {
+        set_csrf_token();
+
+        $operator = $request->attributes->get('_operator');
+        $message_id = $request->attributes->get('message_id', false);
+        $page = array(
+            // Use errors list stored in the request. We need to do so to have
+            // an ability to pass the request from the "save" action.
+            'errors' => $request->attributes->get('errors', array()),
+        );
+
+        if ($message_id) {
+            // Load existing message
+            $canned_message = load_canned_message($message_id);
+            if (!$canned_message) {
+                $page['errors'][] = getlocal('cannededit.no_such');
+                $message_id = false;
+            } else {
+                $title = $canned_message['vctitle'];
+                $message = $canned_message['vcvalue'];
+            }
+        } else {
+            // Create new message
+            $message = '';
+            $title = '';
+            $page['locale'] = $this->extractLocale($request);
+            $page['groupid'] = $this->extractGroupId($request);
+        }
+
+        // Override message's fields from the request if it's needed. This
+        // case will take place when save handler fails.
+        if ($request->request->has('title')) {
+            $title = $request->request->get('title');
+        }
+        if ($request->request->has('message')) {
+            $message = $request->request->get('message');
+        }
+
+        $page['saved'] = false;
+        $page['key'] = $message_id;
+        $page['formtitle'] = $title;
+        $page['formmessage'] = $message;
+        $page['formaction'] = $request->getBaseUrl() . $request->getPathInfo();
+        $page['title'] = empty($message_id)
+            ? getlocal('cannednew.title')
+            : getlocal('cannededit.title');
+        $page = array_merge($page, prepare_menu($operator, false));
+
+        return $this->render('canned_message_edit', $page);
+    }
+
+    /**
+     * Handles "canned_message_add_save" and "canned_message_edit_save" routes.
+     *
+     * The action processes submitting of the forms which are generated in
+     * "canned_message_add" and "canned_message_edit" routes (see
+     * {@link \Mibew\Controller\CannedMessageController::showEditForm()} for
+     * details).
+     *
+     * @param Request $request
+     * @return string Rendered page content
+     */
+    public function saveAction(Request $request)
+    {
+        csrf_check_token($request);
+
+        $operator = $request->attributes->get('_operator');
+        $message_id = $request->request->get('key');
+        $errors = array();
+
+        $title = $request->request->get('title');
+        if (!$title) {
+            $errors[] = no_field("form.field.title");
+        }
+
+        $message = $request->request->get('message');
+        if (!$message) {
+            $errors[] = no_field("form.field.message");
+        }
+
+        if (count($errors) != 0) {
+            $request->attributes->set('errors', $errors);
+
+            // The form should be rebuild. Invoke appropriate action.
+            return $this->showEditFormAction($request);
+        }
+
+        if ($message_id) {
+            save_canned_message($message_id, $title, $message);
+        } else {
+            $locale = $this->extractLocale($request);
+            $group_id = $this->extractGroupId($request);
+            add_canned_message($locale, $group_id, $title, $message);
+        }
+        $page['saved'] = true;
+        $page = array_merge($page, prepare_menu($operator, false));
+
+        return $this->render('canned_message_edit', $page);
+    }
+
+    /**
+     * Extracts locale code from the request.
+     *
+     * @param Request $request
+     * @return string|boolean Locale code or boolean false if the code cannot be
+     * extracted.
+     */
+    protected function extractLocale(Request $request)
+    {
+        $lang = $request->isMethod('POST')
+            ? $request->request->get('lang')
+            : $request->query->get('lang');
+
+        $all_locales = get_available_locales();
+        $correct_locale = !empty($lang)
+            && preg_match("/^[\w-]{2,5}$/", $lang)
+            && in_array($lang, $all_locales);
+        if (!$correct_locale) {
+            return false;
+        }
+
+        return $lang;
+    }
+
+    /**
+     * Extracts group ID from the request.
+     *
+     * @param Request $request
+     * @return string|boolean Group ID or boolean false if the ID cannot be
+     * extracted.
+     */
+    protected function extractGroupId(Request $request)
+    {
+        $group_id = $request->isMethod('POST')
+            ? $request->request->get('group')
+            : $request->query->get('group');
+
+        if (!$group_id) {
+            return false;
+        }
+
+        if (!preg_match("/^\d{0,10}$/", $group_id)) {
+            return false;
+        }
+
+        return $group_id;
     }
 }
