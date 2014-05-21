f4129ff8ee0985f67fe03c2633f2c80e60f23e73
Dmitriy Simushev <simushevds@ossg.ru>
2014-05-21 08:23:51 +0000

    Replace "operator/invite.php" with a controller

---
+++ src/mibew/libs/classes/Mibew/Controller/InvitationController.php
@@ -0,0 +1,58 @@
+<?php
+/*
+ * Copyright 2005-2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Mibew\Controller;
+
+use Mibew\Http\Exception\BadRequestException;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Contains actions which are related with invitations.
+ */
+class InvitationController extends AbstractController
+{
+    /**
+     * Invites a visitor to chat.
+     *
+     * @param Request $request
+     * @return string Rendered page content
+     */
+    public function inviteAction(Request $request)
+    {
+        $operator = $request->attributes->get('_operator');
+
+        // Get visitor ID from the request and check it
+        $visitor_id = $request->query->get('visitor');
+        if (!preg_match("/^\d{1,10}$/", $visitor_id)) {
+            throw new BadRequestException('Wrong format of visitor param.');
+        }
+
+        // Try to invite the visitor
+        $thread = invitation_invite($visitor_id, $operator);
+        if (!$thread) {
+            return new Response('Invitation failed!');
+        }
+
+        // Open chat window for operator
+        $redirect_to = $request->getBasePath()
+            . '/operator/agent.php?thread=' . intval($thread->id)
+            . '&token=' . urlencode($thread->lastToken);
+
+        return $this->redirect($redirect_to);
+    }
+}
