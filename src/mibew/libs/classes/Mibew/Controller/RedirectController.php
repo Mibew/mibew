1a60aeac2e62778558b34032e50b4ef4b1fddaa0
Dmitriy Simushev <simushevds@ossg.ru>
2014-07-14 11:02:20 +0000

    Remove trailing slash if it exists in a request
    Fixes #36

---
+++ src/mibew/libs/classes/Mibew/Controller/RedirectController.php
@@ -0,0 +1,41 @@
+<?php
+/*
+ * Copyright 2005-2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Mibew\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Process all system level redirect actions.
+ */
+class RedirectController extends AbstractController
+{
+    /**
+     * Redirect a user to the URL without trailing slash with 301 HTTP code.
+     *
+     * @param Request $request Incoming request
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     */
+    public function removeTrailingSlashAction(Request $request)
+    {
+        $path_info = $request->getPathInfo();
+        $request_uri = $request->getRequestUri();
+        $url = str_replace($path_info, rtrim($path_info, ' /'), $request_uri);
+
+        return $this->redirect($url, 301);
+    }
+}
