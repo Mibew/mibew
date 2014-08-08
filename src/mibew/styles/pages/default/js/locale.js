f8b5cdafa2ee16557165425306ec65858fac0f7f
Dmitriy Simushev <simushevds@ossg.ru>
2014-08-08 11:49:26 +0000

    Move JavaScript files related with style to style

--- src/mibew/styles/pages/default/js/locale.js
+++ src/mibew/styles/pages/default/js/locale.js
@@ -1,3 +1,11 @@
+/**
+ * @preserve Copyright 2005-2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ */
+
 var popupStatus = 0;
 
 function loadPopup(){
