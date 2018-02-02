048e2dd3f0fb0dc30481af37fc034d155b7d1762
Fedor A. Fetisov <faf@ossg.ru>
2018-02-02 19:19:58 +0300

    Update copyright notices
    Actualize the years

--- src/mibew/libs/classes/Mibew/Controller/ButtonCodeController.php
+++ src/mibew/libs/classes/Mibew/Controller/ButtonCodeController.php
@@ -2,7 +2,7 @@
 /*
  * This file is a part of Mibew Messenger.
  *
- * Copyright 2005-2017 the original author or authors.
+ * Copyright 2005-2018 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
