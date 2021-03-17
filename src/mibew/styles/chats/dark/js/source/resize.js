c8df65baeeb71fc95dce98e0da636080db74cd0c
Fedor A. Fetisov <faf@ossg.ru>
2021-03-17 15:03:43 +0300

    Add dark styles

---
+++ src/mibew/styles/chats/dark/js/source/resize.js
@@ -0,0 +1,144 @@
+/*!
+ * This file is a part of Mibew Messenger.
+ *
+ * Copyright 2005-2021 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+(function(Mibew, $){
+
+    /**
+     * Total height of elements exclude #messages-region or false if it is not
+     * calculated yet
+     * @type Number|Boolean
+     */
+    var delta = false;
+
+    /**
+     * Timer id
+     * @type Number
+     */
+    var t;
+
+    /**
+     * Stretch #messages-region to fill the window.
+     *
+     * @param {Boolean} recalculateHeight Indicates if height of elements must
+     *   be recalculated. It can be useful when elements set is changed.
+     */
+    var updateHeight = function(recalculateHeight) {
+        if ($('#messages-region').size() == 0) {
+            return;
+        }
+        // Create shortcut for #messages-region
+        var $msgs = $('#messages-region');
+        var $ava = $('#avatar-region');
+
+        // Calculate delta
+        if (delta === false || recalculateHeight) {
+            var max = 0;
+            $('body > *').each(function() {
+                var $el = $(this);
+                var pos = $el.offset();
+                var height = $el.height();
+                if (max < (pos.top + height)) {
+                    max = pos.top + height;
+                }
+            });
+            delta = max - $msgs.height();
+        }
+
+        // Check new height
+        var newHeight = $(window).height() - delta;
+
+        if (newHeight < parseInt($msgs.css('minHeight'))) {
+            return;
+        }
+
+        // Update messages region height
+        $msgs.height(newHeight);
+
+        // Update avatar region height
+        if ($ava.size() > 0) {
+            $ava.height($msgs.innerHeight());
+        }
+    }
+
+    /**
+     * Fix bug with window resize event.
+     *
+     * @param {Boolean} recalculateHeight Indicates if height of elements must
+     *   be recalculated. The default value is false.
+     */
+    var updateHeightWrapper = function(recalculateHeight) {
+        if (t) {
+            clearTimeout(t);
+        }
+        t = setTimeout(function() {
+            updateHeight(recalculateHeight || false);
+        }, 0);
+    }
+
+    // Stretch messages region after chat page initialize
+    Mibew.Application.Chat.addInitializer(function() {
+        /**
+         * Contains total count of images on the page
+         * @type Number
+         */
+        var totalImagesCount = $('img').size();
+
+        /**
+         * Contains count of loaded images
+         * @type Number
+         */
+        var imagesLoaded = 0;
+
+        /**
+         * Run then image loaded. If all images loaded run resizer.
+         * @type Function
+         */
+        var imageLoadCallback = function() {
+            imagesLoaded++;
+            if (totalImagesCount == imagesLoaded) {
+                updateHeight();
+                // Scroll messages region to bottom
+                $('#messages-region').scrollTop(
+                    $('#messages-region').prop('scrollHeight')
+                );
+                // Stretch messages region on resize
+                $(window).resize(updateHeightWrapper);
+            }
+        }
+
+        // Change size of message region only after all images will be loaded
+        $('img').each(function(){
+            // Shortcut for current image element
+            var $el = $(this);
+            // Check if image already loaded and cached.
+            // Cached image have height and do not triggers load event.
+            if ($el.height() > 0) {
+                imageLoadCallback();
+            } else {
+                $el.load(imageLoadCallback);
+            }
+        });
+
+        // Change of user's "canPost" field changes visibility of message input
+        // form. Thus we should manually update the height.
+        Mibew.Objects.Models.user.on('change:canPost', function() {
+            updateHeightWrapper(true);
+        });
+    });
+
+})(Mibew, jQuery);
---
+++ src/mibew/styles/pages/dark/js/locale.js
@@ -0,0 +1,82 @@
+/*!
+ * This file is a part of Mibew Messenger.
+ *
+ * Copyright 2005-2021 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+(function($, document) {
+    var isPopupOpened = false;
+
+    var loadPopup = function(){
+        if(!isPopupOpened){
+            $("#background-popup").css({
+                "opacity": "0.7"
+            });
+            $("#background-popup").fadeIn("slow");
+            $("#dashboard-locales-popup").fadeIn("slow");
+            isPopupOpened = true;
+        }
+    }
+
+    var disablePopup = function(){
+        if(isPopupOpened){
+            $("#background-popup").fadeOut("slow");
+            $("#dashboard-locales-popup").fadeOut("slow");
+            isPopupOpened = false;
+        }
+    }
+
+    var normalizePosition = function(a) {
+        if(a < 10) {
+            return 10;
+        }
+        return a;
+    }
+
+    var centerPopup = function(){
+        var windowWidth = document.documentElement.clientWidth;
+        var windowHeight = document.documentElement.clientHeight;
+        var popupHeight = $("#dashboard-locales-popup").height();
+        var popupWidth = $("#dashboard-locales-popup").width();
+        $("#dashboard-locales-popup").css({
+            "position": "absolute",
+            "top": normalizePosition((windowHeight-popupHeight) * 0.2),
+            "left": normalizePosition(windowWidth/2-popupWidth/2)
+        });
+        $("#background-popup").css({
+            "height": windowHeight
+        });
+    }
+
+    $(function(){
+        $("#change-language").on('click', function(){
+            centerPopup();
+            loadPopup();
+            return false;
+        });
+        $("#dashboard-locales-popup-close").on('click', function(){
+            disablePopup();
+            return false;
+        });
+        $("#background-popup").on('click', function(){
+            disablePopup();
+        });
+        $(document).keypress(function(e){
+            if(e.keyCode == 27 && isPopupOpened){
+                disablePopup();
+            }
+        });
+    });
+})(jQuery, document);
