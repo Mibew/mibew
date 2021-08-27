99fb30b0ec01fd2dc6e905d3a34b192e9a1fe544
Fedor A. Fetisov <faf@ossg.ru>
2021-08-27 13:03:28 +0300

    Remove redundant legacy code

--- src/mibew/libs/common/string.php
+++ src/mibew/libs/common/string.php
@@ -17,29 +17,6 @@
  * limitations under the License.
  */
 
-function unicode_urldecode($url)
-{
-    preg_match_all('/%u([[:alnum:]]{4})/', $url, $a);
-
-    foreach ($a[1] as $uniord) {
-        $dec = hexdec($uniord);
-        $utf = '';
-
-        if ($dec < 128) {
-            $utf = chr($dec);
-        } elseif ($dec < 2048) {
-            $utf = chr(192 + (($dec - ($dec % 64)) / 64));
-            $utf .= chr(128 + ($dec % 64));
-        } else {
-            $utf = chr(224 + (($dec - ($dec % 4096)) / 4096));
-            $utf .= chr(128 + ((($dec % 4096) - ($dec % 64)) / 64));
-            $utf .= chr(128 + ($dec % 64));
-        }
-        $url = str_replace('%u' . $uniord, $utf, $url);
-    }
-    return urldecode($url);
-}
-
 function cut_string($string, $length = 75, $ellipsis = '')
 {
     $result = '';
